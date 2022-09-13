1a46a6e29c7c464c9a9285d1863d677c08b4091f
Burcu Saglam <saglamburcu96@gmail.com>
2022-09-13 21:18:54 +0300

    Delete the product and get product details

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -47,8 +47,46 @@ const updateProduct = async (req, res) => {
   })
 }
 
+// delete product -- Admin
+const deleteProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(500).json({
+      success: false,
+      message: "Product is not found with this id"
+    })
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully"
+  })
+}
+
+// single product detail
+const getProductDetail = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(500).json({
+      success: false,
+      message: "Product is not found with this id"
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
+
 module.exports = {
   getAllProducts,
   createProduct,
-  updateProduct
+  updateProduct,
+  deleteProduct,
+  getProductDetail
 }
--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct } = require("../controller/ProductController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetail } = require("../controller/ProductController");
 
 const router = express.Router();
 
 router.get("/products", getAllProducts);
 router.post("/product/new", createProduct);
 router.put("/product/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
+router.get("/product/:id", getProductDetail);
 
 module.exports = router;
