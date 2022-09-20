eefcf7e983bdc9481553b4c4efcaa53b5f83c55c
Burcu Saglam <saglamburcu96@gmail.com>
2022-09-20 15:50:06 +0300

    Create Header component

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.scss";
+
+const images = [
+  {url: "images/photo1.jpg"},
+  {url: "images/photo2.jpg"}
+]
+
+const Home = () => {
+  return (
+    <div className="home">
+      Slider
+    </div>
+  )
+}
+
+export default Home;
