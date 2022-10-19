5c167336fcc3590ba086f13e7ac9edfc3240af24
Burcu Saglam <saglamburcu96@gmail.com>
2022-10-19 18:00:41 +0300

    Fix footer UI

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,23 +1,48 @@
 import "./Footer.scss";
 import MenuItems from "../HomePageComponents/MenuItems/MenuItems";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer__address">
-        <h1>PatiShop</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sed eius vel unde possimus perferendis voluptate saepe maiores, commodi vero sapiente placeat cumque repellat quaerat eos minus mollitia consequatur molestias!
+      <div className="footer__contact">
+        <img src="images/logo.png" alt="" />
+
+        <div className="footer__contact__address">
+          <FontAwesomeIcon icon={faLocationDot} className="footer__contact__address__icon" />
+          <div>
+            Bostanlı Mah. Şehitler Bulvarı No: 52/A <br />
+            Karşıyaka / İZMİR
+          </div>
+        </div>
+
+        <div className="footer__contact__phone">
+          <FontAwesomeIcon icon={faPhone} className="footer__contact__phone__icon" />
+
+          <div>(0232) XXX YY ZZ</div>
+        </div>
       </div>
       <div className="footer__quick__menu">
-        <h3>Menü</h3>
-        <MenuItems />
+        <ul>
+          <MenuItems />
+        </ul>
       </div>
-      <ul className="footer__follow__us">
+      <div className="footer__follow__us">
         <h3>Bizi Takip Edin</h3>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Twitter</li>
-      </ul>
+        <ul>
+          <li>
+            <BsFacebook className="facebook" />
+          </li>
+          <li>
+            <BsInstagram className="instagram" />
+          </li>
+          <li>
+            <BsTwitter className="twitter" />
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
