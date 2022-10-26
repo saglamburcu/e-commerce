29ed7cec38b68f58175d8fa9b8562e0905619647
Burcu Saglam <saglamburcu96@gmail.com>
2022-10-26 16:10:11 +0300

    Fix Favorites Page UI

--- frontend/src/components/BasketPageComponents/MyBasket/MyBasket.jsx
+++ frontend/src/components/BasketPageComponents/MyBasket/MyBasket.jsx
@@ -1,35 +1,46 @@
 import "./MyBasket.scss";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const MyBasket = ({showBtn, productsList, removeProduct, singleProductButton, isBasketPage, isFavoritePage, isOrderConfirm, addToBasketFromFavorite}) => {
+const MyBasket = ({ showBtn, productsList, removeProduct, singleProductButton, isBasketPage, isFavoritePage, isOrderConfirm, addToBasketFromFavorite }) => {
   const navigate = useNavigate();
 
   return (
     <div className="mybasket">
-      <div className="mybasket__products">
+      <table className="mybasket__products">
         {
           productsList?.map(product => {
 
             return (
-                <div className="mybasket__products__item">
-                  <img src={product.productInfos ? product.productInfos.images[0].url : product.image} style={{width:"100px"}} alt="" />
+              <tr>
+                <td className="mybasket__products__image">
+                  <img src={product.productInfos ? product.productInfos.images[0].url : product.image} alt="" />
+                </td>
+                <td className="mybasket__products__name">
                   <h5>{product.productInfos ? product.productInfos.name : product.name}</h5>
+                </td>
+                <td className="mybasket__products__quantity">
                   <h3>{product.count ? product.count : product.quantity} adet</h3>
+                </td>
+                <td className="mybasket__products__price">
                   <h3>{product.count ? product?.count * product.productInfos?.price : product.quantity * product.price} TL</h3>
+                </td>
+                <td className="mybasket__products__buttons">
                   {
-                    showBtn && 
-                      <button type="button" onClick={() => removeProduct(product.productInfos._id)}>{singleProductButton}</button>
+                    showBtn &&
+                    <button type="button" onClick={() => removeProduct(product.productInfos._id)}>{singleProductButton}</button>
                   }
 
                   {
-                    isFavoritePage && 
-                      <button onClick={() => addToBasketFromFavorite(product.productInfos._id, productsList)}>Sepete Ekle</button>
+                    isFavoritePage &&
+                    <button onClick={() => addToBasketFromFavorite(product.productInfos._id, productsList)}>Sepete Ekle</button>
                   }
-                </div>
+                </td>
+              </tr>
             )
           })
         }
-      </div>
+      </table>
+
 
       {
         isBasketPage && (
@@ -38,7 +49,7 @@ const MyBasket = ({showBtn, productsList, removeProduct, singleProductButton, is
           </div>
         )
       }
-    </div>
+    </div >
   )
 }
 
