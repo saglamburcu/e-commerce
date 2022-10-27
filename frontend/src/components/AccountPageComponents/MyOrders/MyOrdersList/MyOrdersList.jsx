ec724a45452b2b2667db2029573fcdfb61d4eecd
Burcu Saglam <saglamburcu96@gmail.com>
2022-10-27 15:17:00 +0300

    Fix Account - My Orders Page UI

--- frontend/src/components/AccountPageComponents/MyOrders/MyOrdersList/MyOrdersList.jsx
+++ frontend/src/components/AccountPageComponents/MyOrders/MyOrdersList/MyOrdersList.jsx
@@ -1,13 +1,13 @@
 import "./MyOrdersList.scss";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { fetchAllOrders } from "../../../../api";
-import {UserContext} from "../../../../context/UserContext";
+import { UserContext } from "../../../../context/UserContext";
 
 const MyOrders = () => {
-  const {userInfo} = useContext(UserContext)
+  const { userInfo } = useContext(UserContext)
   const [allOrders, setAllOrders] = useState([]);
 
   useEffect(() => {
@@ -17,39 +17,39 @@ const MyOrders = () => {
     })()
   }, []);
 
-  console.log(allOrders)
-
   return (
-    <table>
-      <tr>
-        <th>Sipariş Tarihi</th>
-        <th>Sipariş Miktarı</th>
-        <th>Alıcı</th>
-        <th>Teslimat Adresi</th>
-        <th>Tutar</th>
-        <th>Sipariş Detayı</th>
-      </tr>
-      {
-        allOrders.map(order => {
-          const {country, state, city, address} = order.shippingInfo;
-          
-          return (
-            <tr>
-              <td>{order.createdAt.split("T")[0]}</td>
-              <td>{order.orderItems.reduce((cumulative, item) => cumulative + item.quantity, 0)} Ürün</td>
-              <td>{userInfo.name}</td>
-              <td>{country} {state} {city} {address}</td>
-              <td>{order.totalPrice} TL</td>
-              <td>
-                <Link to={`/order/${order._id}`}>
-                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </Link>
-              </td>
-            </tr>
-          )
-        })
-      }
-    </table>
+    <div className="myOrdersList">
+      <table>
+        <tr>
+          <th>Sipariş Tarihi</th>
+          <th>Sipariş Miktarı</th>
+          <th>Alıcı</th>
+          <th>Teslimat Adresi</th>
+          <th>Tutar</th>
+          <th>Sipariş Detayı</th>
+        </tr>
+        {
+          allOrders.map(order => {
+            const { country, state, city, address } = order.shippingInfo;
+
+            return (
+              <tr>
+                <td>{order.createdAt.split("T")[0]}</td>
+                <td>{order.orderItems.reduce((cumulative, item) => cumulative + item.quantity, 0)} Ürün</td>
+                <td>{userInfo.name}</td>
+                <td>{country} {state} {city} {address}</td>
+                <td>{order.totalPrice} TL</td>
+                <td>
+                  <Link to={`/order/${order._id}`} className="details__icon">
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                  </Link>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </table>
+    </div>
   )
 }
 
